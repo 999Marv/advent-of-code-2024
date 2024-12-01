93ea8d10778c8c0536d1681ef9479135ed771931
999Marv <marvinsiri123@gmail.com>
2024-12-01 13:42:33 -0500

    refactor

--- day1/day1.js
+++ day1/day1.js
@@ -5,30 +5,34 @@ function parseInput(path) {
   return fileContents.split("\n");
 }
 
-function main() {
-  const inputPath = "./day1/day1.txt";
-  const lines = parseInput(inputPath);
-
+function shapeArray(lines) {
   const arr1 = [];
   const arr2 = [];
 
   lines.forEach((line) => {
     const [val1, val2] = line.split("   ");
-
-    arr1.push(val1);
-    arr2.push(val2);
+    arr1.push(Number(val1));
+    arr2.push(Number(val2));
   });
 
   arr1.sort((a, b) => a - b);
   arr2.sort((a, b) => a - b);
 
-  let count = 0;
+  return { arr1, arr2 };
+}
 
-  for (let i = 0; i < arr1.length; i++) {
-    count += Math.abs(arr1[i] - arr2[i]);
-  }
+function main() {
+  const inputPath = "./day1/day1.txt";
+  const lines = parseInput(inputPath);
 
-  console.log(count);
+  const { arr1, arr2 } = shapeArray(lines);
+
+  const ans = arr1.reduce(
+    (sum, val, idx) => sum + Math.abs(val - arr2[idx]),
+    0
+  );
+
+  console.log(ans);
 }
 
 main();
